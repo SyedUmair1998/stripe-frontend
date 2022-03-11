cf1ebc736920b1cf3c454c5138efb435fe651de7
SyedUmair1998 <syedumairh1998@gmail.com>
2022-03-11 12:50:08 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import StripeCheckout from 'react-stripe-checkout';
+import React from 'react';
+
 
 function App() {
+  
+  
+  const [product,setProduct] = React.useState({
+    price:100,
+  });
+  
+  
+  
+  const makePayment = (token) => {
+    const body  = {
+      token,
+      product
+    }
+    const headers = {
+      "Content-Type":"application/json"
+    }
+    return fetch(`http://localhost:3000/payment`,{
+      method:"POST",
+      headers,
+      body:JSON.stringify(body) 
+    })
+    .then(response =>{
+      console.log(response);
+      const {status} = response;
+      console.log(status);
+    })
+    .catch(err=>console.log(err));
+    
+    
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +50,10 @@ function App() {
         >
           Learn React
         </a>
+      <StripeCheckout stripeKey='pk_test_51Kc2FWCxOg6ThvoBrQoRkcJuWyQOdIca8UkGo5jjfrU2zqYGyU2fTCRx45rZaTFbKtkZhPBQBGW0ECXjVB2EFNod00vGbUuUis'
+       token={makePayment}
+      name="By React"
+      amount={product.price} />
       </header>
     </div>
   );
